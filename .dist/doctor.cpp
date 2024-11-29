1f90ae503959dc77b445b10d9a43f4047917eb97
youssefsaleh61 <youssefsaleh61@yahoo.com>
2024-11-29 16:42:00 +0200

    add doctor table and initialize doctors file

--- .dist/doctor.cpp
+++ .dist/doctor.cpp
@@ -1,14 +1,74 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
 using namespace std;
 class Doctor{
-    string doctor_id;
-    string doctor_name;
-    string doctor_address;
+    private:
+        int doctor_length;
+        char doctor_id[15] = {' '};
+        char doctor_name[30] = {' '};
+        char doctor_address[30] = {' '};
+    public:
+        char* getID() {
+            return doctor_id;
+        }
+        char* getName() {
+            return doctor_name;
+        }
+        char* getAddress() {
+            return doctor_address;
+        }
+        int getLength() {
+            return doctor_length;
+        }
+        Doctor(string id, string name, string address) {
+            strcpy(doctor_id, id.c_str());
+            strcpy(doctor_name, name.c_str());
+            strcpy(doctor_address, address.c_str());
+            doctor_length = strlen(doctor_id) + strlen(doctor_name) + strlen(doctor_address) + 2;
+        }
+        Doctor() {
+            cin.ignore();
+            cout << "Enter ID: ";
+            cin.getline(doctor_id, 15);
+            cout << "Enter Name: ";
+            cin.getline(doctor_name, 30);
+            cout << "Enter Address: ";
+            cin.getline(doctor_address, 30);
+            doctor_length = strlen(doctor_id) + strlen(doctor_name) + strlen(doctor_address) + 2;
+        }
+};
 
-    Doctor(string id, string name, string address){
-        doctor_id=id;
-        doctor_name=name;
-        doctor_address=address;
-    }
-
+class DoctorTable{
+    private:
+        int AVAILLIST = -1;
+        fstream file;
+        fstream filePrimaryIndex;
+        fstream fileSecondaryIndex;
+    public:
+        DoctorTable() {
+            ofstream output("doctors.txt", ios::app);
+            output.close();
+            char READAVAILLIST[5];
+            file.open("doctors.txt", ios::in);
+            file.getline(READAVAILLIST, 5);
+            if (strlen(READAVAILLIST) > 0) {
+                AVAILLIST = stoi(READAVAILLIST);
+            }
+            file.close();
+            file.open("doctors.txt", ios::in | ios::out);
+            file.seekp(0, ios::beg);
+            file << setw(5) << setfill(' ') << left << AVAILLIST << '\n';
+            file.close();
+        }
+        int getAVAILLIST() {
+            return AVAILLIST;
+        }
+        void AddDoctor(Doctor d) {
+            file.open("doctors.txt", ios::app | ios::out);
+            file << setw(2) << setfill('0') << d.getLength();
+            file << d.getID() << '|' << d.getName() << '|' << d.getAddress() << '\n';
+            file.close();
+        }
 };
--- .dist/main.cpp
+++ .dist/main.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
+#include "doctor.cpp"
 using namespace std;
 class HealthCareManagementSystem{
-    void mainMenu(){
-        while (true) {
-            cout<<"1- Welcome to Healthcare mangement system";
-            cout<<"2- Add new doctor";
-            cout<<"3- Add new appointment";
-            cout<<"4- Update Doctor Name (Doctor ID)";
-            cout<<"5- Update Appointment Date (Appointment ID)";
-            cout<<"6- Delete Appointment (Appointment ID)";
-            cout<<"7- Delete Doctor (Doctor ID)";
-            cout<<"8- Print Doctor Info (Doctor ID)";
-            cout<<"9- Print Appointment Info (Appointment ID)";
-            cout<<"10- Write Query";
-            cout<<"0- Exit";
-        
+    private:
+        DoctorTable dt;
+        void mainMenu(){
+            cout<<"1-Welcome to Healthcare mangement system\n";
+            cout<<"2-Add new doctor\n";
+            cout<<"3-Add new appointment\n";
+            cout<<"4-Update Doctor Name (Doctor ID)\n";
+            cout<<"5-Update Appointment Date (Appointment ID)\n";
+            cout<<"6-Delete Appointment (Appointment ID)\n";
+            cout<<"7-Delete Doctor (Doctor ID)\n";
+            cout<<"8-Print Doctor Info (Doctor ID)\n";
+            cout<<"9-Print Appointment Info (Appointment ID)\n";
+            cout<<"10-Write Query\n";
+            cout<<"0-Exit\n";
+        } 
+    public:
+        HealthCareManagementSystem() {
+            dt = DoctorTable();
         }
-    }
-
+        
+        void Run() {
+            int input;
+            while (true) {
+                mainMenu();
+                cin >> input;
+                if (input == 0) {
+                    break;
+                }
+                switch (input) {
+                    case 2:
+                        dt.AddDoctor(Doctor());
+                }
+            }
+        }
 };
+
+
+int main() {
+    HealthCareManagementSystem HCMS;
+    HCMS.Run();
+
+    return 0;
+}
